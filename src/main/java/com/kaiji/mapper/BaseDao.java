f2be0bfe9ffa8df786d8e6a197f17cb94bc465fc
开机 <han_wuhan@sina.com>
2018-11-03 13:56:51 +0800

    修改pom文件spring版本, 修复版本漏洞

--- src/main/java/com/kaiji/mapper/BaseDao.java
+++ src/main/java/com/kaiji/mapper/BaseDao.java
@@ -3,8 +3,11 @@ package com.kaiji.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
 import tk.mybatis.mapper.common.BaseMapper;
 
+@Component
 public interface BaseDao<T> extends BaseMapper<T> {
 
 	List<T> selectByFilter(Map<String, Object> filterMap);
